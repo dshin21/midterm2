446465c198c698565c1fc67e866f03f39a5b33c8
Daniel Shin <danielshin234@gmail.com>
2018-11-26 10:09:05 -0800

    init

---
+++ cmake-build-debug/CMakeFiles/midterm2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/midterm2.dir/main.cpp.o"
+  "midterm2.pdb"
+  "midterm2.exe"
+  "libmidterm2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/midterm2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
